e350a5cd81d2aa079333b6a9da94d64508b53a80
farhanmaneri <farhanmaneri@gmail.com>
2020-06-12 21:40:15 -0700

    js updated

--- app.js
+++ app.js
@@ -1,127 +1,277 @@
-// // chapter 1
-alert("Error! Please enter a valid passward");
+// // // chapter 1
+// alert("Error! Please enter a valid passward");
 
-alert("Welcome to JS Land..." +   "Happy Coding!"); 
+// alert("Welcome to JS Land..." +   "Happy Coding!"); 
 
-alert("Welcome to Js Land...")
+// alert("Welcome to Js Land...")
 
-alert("Happy codding!" + "<br>" + "Prevent this page from creating additional dialogs")
+// alert("Happy codding!" +  "Prevent this page from creating additional dialogs");
 
-alert("Hello.. I can run JS through web browser console")
-console.log(alert)
-
-
-// // chapter 2
-
-//  var userName;
-//  myName = "Farhan Khan";
-
- var message;
- message = "Hello World!";
-alert(message);
-
-// var student;
-// name = "Jhone Doe";
-// age = "45 years old"
-// certificate = "certified Mobile Application Development"
-// alert(studnet);
-// document.write(alert);
-
-var  x = "pizza";
-for (var i = 5;  i > 0; i--) {
-    for ( var j = 0; j< i; j++){
-        document.write(x[j]);
-    }
-    document.write("<br>"); 
-}
+// alert("Hello.. I can run JS through web browser console")
+// console.log(alert)
 
 
-var email = "farhanmaneri@gmail.com.";
-alert("My email address is" + " " + email);
+// // // chapter 2
 
-var book = "A Smarter way to learn JavaScript";
-alert( "I am trying to learn from the book" + " " + book);
+// //  var userName;
+// //  myName = "Farhan Khan";
+
+//  var message;
+//  message = "Hello World!";
+// alert(message);
+
+// // var student;
+// // name = "Jhone Doe";
+// // age = "45 years old"
+// // certificate = "certified Mobile Application Development"
+// // alert(studnet);
+// // document.write(alert);
+
+// var  x = "pizza";
+// for (var i = 5;  i > 0; i--) {
+//     for ( var j = 0; j< i; j++){
+//         document.write(x[j]);
+//     }
+//     document.write("<br>"); 
+// }
+
+
+// var email = "farhanmaneri@gmail.com.";
+// alert("My email address is" + " " + email);
+
+// var book = "A Smarter way to learn JavaScript";
+// alert( "I am trying to learn from the book" + " " + book);
   
-var  message = "Yah! I can write HTML content through JavaScript.";
-document.write(message);
+// var  message = "Yah! I can write HTML content through JavaScript.";
+// document.write(message);
 
-var end = "---------*******@@@@@@*******----------"
-alert(end);
+// var end = "---------*******@@@@@@*******----------"
+// alert(end);
 
-// chapter 3
+// // chapter 3
 
- var age = "I am 15 years old";
- alert(age);
+//  var age = "I am 15 years old";
+//  alert(age);
 
-var birthYear = "1988";
-document.write("My birth Year is" + birthYear + "<br>" + "Date type of my date of birth is number");
+// var birthYear = "1988";
+// document.write("My birth Year is" + birthYear + "<br>" + "Date type of my date of birth is number");
 
 
-// chapter 4
+// // chapter 4
 
-// 5 legal variables = letters, numbers, dollor sign and underscore.
-// 5 illegal variabls = userAlert, myVar, for, false. true.
+// // 5 legal variables = letters, numbers, dollor sign and underscore.
+// // 5 illegal variabls = userAlert, myVar, for, false. true.
  
-var  heading = "Rules for naming JS Variable" ;
-    document.write( heading  + "<br>");
-var variableNames = "Variable names can only cotain letters, numbers, dollor sign and underscore.";
-     document.write(variableNames + "<br>" + "for example: $my_1stVariabel");
-var variableRules = "Variables must begin with letters, $ or _"
-    document.write(variableRules + "<br>" + "for example $name or name." + "<br>" + "variable names are Case Sensitive." + "<br>" + "variable names should not be JS keywords")
+// var  heading = "Rules for naming JS Variable" ;
+//     document.write( heading  + "<br>");
+// var variableNames = "Variable names can only cotain letters, numbers, dollor sign and underscore.";
+//      document.write(variableNames + "<br>" + "for example: $my_1stVariabel");
+// var variableRules = "Variables must begin with letters, $ or _"
+//     document.write(variableRules + "<br>" + "for example $name or name." + "<br>" + "variable names are Case Sensitive." + "<br>" + "variable names should not be JS keywords")
  
-// Chapter 5
-// 1.
-var x = +prompt("enter value");
-var sign = prompt("enter operater");
-var y = +prompt("enter value");
+// // Chapter 5
+// // 1.
+// var x = +prompt("enter value");
+// var sign = prompt("enter operater");
+// var y = +prompt("enter value");
 
- if ( sign === "+"){
-     document.write("Sum of" + " " + x + " " + "and"+ " " + y + "is" + " " +(+x)+(+y));
- }else if( sign === "-"){
-    document.write(x-y);
- }else if( sign === "*"){
-     document.write(x +" " + "*" + " " + y + " " + "is" + " " + x*y);
+//  if ( sign === "+"){
+//      document.write("Sum of" + " " + x + " " + "and"+ " " + y + "is" + " " +(+x)+(+y));
+//  }else if( sign === "-"){
+//     document.write(x-y);
+//  }else if( sign === "*"){
+//      document.write(x +" " + "*" + " " + y + " " + "is" + " " + x*y);
 
- }else if ( sign === "/"){
-      document.write( x + "/" + y + " "+ "is" + " " + x/y);
-    }else if ( sign === "%"){
-      document.write( x + "%" + y + " "+ "is" + " " + x%y);
-  }
+//  }else if ( sign === "/"){
+//       document.write( x + "/" + y + " "+ "is" + " " + x/y);
+//     }else if ( sign === "%"){
+//       document.write( x + "%" + y + " "+ "is" + " " + x%y);
+//   }
 
-// Q 4.
-        var movieTicket = 600;
-        var quantity = 5;
-        document.write("Total cost to buy"+ " " + quantity + " "+ "ticket to a movie is" + " "+ movieTicket*quantity + "PKR"+ "<br>")
-//  // Q 5.
-        for(var i = 1; i <= 10; i++){
+// // Q 4.
+//         var movieTicket = 600;
+//         var quantity = 5;
+//         document.write("Total cost to buy"+ " " + quantity + " "+ "ticket to a movie is" + " "+ movieTicket*quantity + "PKR"+ "<br>")
+// //  // Q 5.
+//         for(var i = 1; i <= 10; i++){
             
-               document.write( "4" + "x" + i + "=" + 4*i + "<br>") ;
-    }
+//                document.write( "4" + "x" + i + "=" + 4*i + "<br>") ;
+//     }
 
-// // Q 7.
-        var priceOfItem1    = 650;
-        var quantityOfItem1 = 3;
-        var priceOfItem2    = 100;
-        var quantityOfItem2  = 7;
-        var shippingCharges = 100;
-        var firstItem = priceOfItem1*quantityOfItem1;
-        var secondItem = priceOfItem2*quantityOfItem2;
-        document.write("Total cost of your order is" + " " + (((+firstItem) + (+secondItem)) + (+shippingCharges))+ "<br>");
-// Q 8.
-        var totalMarks = 980
-        var marksObt = 804;
-        document.write("Percentage:" + " "+ marksObt/totalMarks*100 + "%" + "<br>");
-// Q 9.
-        var dollorUS = 10;
-        var riyalSaudi = 25;
-        document.write("Total Currency in PKR:" + " "+(( dollorUS*104.80) + (riyalSaudi*28)));
-// Q 10.
-         var x = 1; 
-         x+5;
-         document.write(((x + x+5) * 10) / 2 + "<br>");
-// Q.11.
-        var currentYear = 2016;
-        var birthYear = 1992;
-        document.write("Your age is" + " " + ((currentYear)-(birthYear)));
+// // // Q 7.
+//         var priceOfItem1    = 650;
+//         var quantityOfItem1 = 3;
+//         var priceOfItem2    = 100;
+//         var quantityOfItem2  = 7;
+//         var shippingCharges = 100;
+//         var firstItem = priceOfItem1*quantityOfItem1;
+//         var secondItem = priceOfItem2*quantityOfItem2;
+//         document.write("Total cost of your order is" + " " + (((+firstItem) + (+secondItem)) + (+shippingCharges))+ "<br>");
+// // Q 8.
+//         var totalMarks = 980
+//         var marksObt = 804;
+//         document.write("Percentage:" + " "+ marksObt/totalMarks*100 + "%" + "<br>");
+// // Q 9.
+//         var dollorUS = 10;
+//         var riyalSaudi = 25;
+//         document.write("Total Currency in PKR:" + " "+( ( dollorUS*104.80) + (riyalSaudi*28)));
+// // Q 10.
+//          var x = 1; 
+//          x+5;
+//          document.write(((x + x+5) * 10) / 2 + "<br>");
+// // Q.11.
+//         var currentYear = 2016;
+//         var birthYear = 1992;
+//         document.write("Your age is" + " " + ((currentYear)-(birthYear)));
 
+// Chapter 6-9
+    // task 3
+                // var a = prompt("Enter your name");
+                // document.write("Greeting" + " " + a);
+
+    // task 5
+                // var a = +prompt("enter value");
+                // var b = a;
+                // for( var i = 1; i <= 10; i++){
+                //     if(a === b){
+                //         document.write(b + "x"+ i + "=" + b*i + "<br>");
+                //     } else if ( b === " "){
+                //       document.write("5" + "x" + i + "=" + 5*i + "<br>");
+                //       }
+
+                // }
     
+    
+// chapter 9-10
+    // task 1
+                // var a = prompt( "write you city");
+                // if (a === "karachi"){
+                //     document.write("Welcome to Karachi")
+                // }
+    // task 2   
+            //     var gender = prompt("write your gender");
+              
+            //    if ( gender === "male"){
+            //        document.write( "Good Morning Sir!")
+            //    }else if ( gender === "female"){
+            //        document.write( "Good Morning Ma'am!")
+            //    }
+
+    // task 3
+            //     var signal = prompt("enter signal color");
+            //     if ( signal === "red"){
+            //     document.write("Stop")
+            // }else if ( signal === "yellow"){
+            //     document.write("Ready to Move")
+            // }else if ( signal === "green"){
+            //     document.write( "Move Now")
+            // }
+    // task 4
+                // var fuel = +prompt( "remaning fuel");
+                // if ( fuel <  .25){
+                //     document.write("Please refill the fuel in your car")
+                // }
+    
+    // task 5d
+                // var martialCost = 20000;
+                // var laborCost = 2000;
+                // var totalCost = (+martialCost)+ (+laborCost);
+                // if ( totalCost === laborCost+martialCost){
+                //     alert("The cost equals");
+                // }if(true){
+                //     alert("True");
+                // }if (false){
+                //     alert("false");
+                // }
+
+// Chapter 12-13.
+        // task 4.
+                        // var a = prompt("enter an alphabet");
+                        // if( a === "a" || a === "e" || a === "i" || a === "o" || a === "u") {
+                        //         document.write(true);
+                        // } else  {
+
+                        //         document.write(false);
+
+                        // }
+                               
+// task 4.
+                        // var a = +prompt("enter a number");
+                        // if( a === 0){
+                        //         document.write('number is zero');
+                        // }else if(a > 0){
+                        //         document.write("number is positive")
+                        // }else {
+                        //         document.write("number is negative")
+                        // }
+
+
+//   task 5
+                        // var a = "farhankhan";
+                        // var b = prompt("enter your passward");
+                        // b = b.toLocaleLowerCase();
+
+
+                        // if (a  === " "){
+                        //         alert("Please enter your passward");
+
+                        // }else if( b === a){
+                        //         alert("Correct! The passward you enter matches the original passward");
+                        // }else {
+                        //         alert("Incorrect Passward"); 
+                        // }
+
+        // task 6       
+                        // var greeting;
+                        // var hour = 13;
+                        // if(hour < 18){
+                        //         document.write("Good day");
+                        // }else{
+                        //         document.write("Good evening");
+                        // }
+        // task 7.
+                        // var time = +prompt("Enter time")
+
+                        // if (time >= 0000 && time < 1200){
+                        //         document.write("Good morning");
+                        // }else if(time >= 1200 && time < 1700){
+                        //         document.write('Good afternoon');
+                        
+                        // }else if( time >= 1700 && time < 2100){
+                        //         document.write("Good evening");
+                        // }else if ( time >= 2100 && time <= 2359){
+                        //         document.write("Good night");
+                        // }
+
+// Chapter 17-20.
+
+// task 4
+                // var a = +prompt("enter a number to show its multiplication");
+                // var b = +prompt("enter legth of multiplication table");
+                // for( var i = 1; i <= b; i++){
+                //                 document.write(a + "x"+ i + "=" + a*i + "<br>");
+                // }
+    
+
+        // task 5
+                        // var fruits = ["apple", "banana", "mango", "oroange", "strawberry"];
+                        // document.write(fruits);
+        
+       
+        // for(i = 1; i <= 15; i++){
+        //         document.write(i +",")
+        // }
+
+// // task 7.
+//                 var A = [ "cake", "apple pie", "cookie","chips", "patties"];
+//                 var B = prompt("search");
+//                 for (var i = 0; i < A.length; i++);
+//                 if (B === A[i]){
+//                         document.write("avialable");
+//                 }else{
+//                         document.write("unavailable");
+//                    }
+        
+     
+                
+        
